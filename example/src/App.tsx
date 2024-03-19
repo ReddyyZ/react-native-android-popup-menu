6ff470ff581fd0f8fa0f9aea3637e797b6ef2033
ReddyyZ <arthurc.oli.whitehat@gmail.com>
2024-03-19 19:24:01 -0300

    Creating example gif

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
 
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import PopupMenu from 'react-native-simple-popup-menu';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Text>Highlight mode</Text>
       <PopupMenu
         onPress={(event, index) => {
           if (event === 'itemSelected') {
@@ -16,6 +17,61 @@ export default function App() {
         }}
         onError={() => {}}
         items={['teste']}
+        containerStyle={{
+          margin: 16
+        }}
+        mode="highlight"
+      />
+
+      <Text>Native mode</Text>
+      <PopupMenu
+        onPress={(event, index) => {
+          if (event === 'itemSelected') {
+            console.log('Pressed:', index);
+          } else {
+            console.log('Dismissed');
+          }
+        }}
+        onError={() => {}}
+        items={['teste']}
+        containerStyle={{
+          margin: 16
+        }}
+        mode="native"
+      />
+
+      <Text>Opacity mode</Text>
+      <PopupMenu
+        onPress={(event, index) => {
+          if (event === 'itemSelected') {
+            console.log('Pressed:', index);
+          } else {
+            console.log('Dismissed');
+          }
+        }}
+        onError={() => {}}
+        items={['teste']}
+        containerStyle={{
+          margin: 16
+        }}
+        mode="opacity"
+      />
+
+      <Text>Without Feedback mode</Text>
+      <PopupMenu
+        onPress={(event, index) => {
+          if (event === 'itemSelected') {
+            console.log('Pressed:', index);
+          } else {
+            console.log('Dismissed');
+          }
+        }}
+        onError={() => {}}
+        items={['teste']}
+        containerStyle={{
+          margin: 16
+        }}
+        mode="without-feedback"
       />
     </View>
   );
